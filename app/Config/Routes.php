09ad295c194158bcbffb2c3eafc99f759c5b6681
fridokasman-cpu <fridolinkasman@gmail.com>
2026-05-10 16:02:39 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/mahasiswa', 'Mahasiswa::index');
+$routes->get('/mahasiswa/getData', 'Mahasiswa::getData');
+$routes->post('/mahasiswa/simpan', 'Mahasiswa::simpan');
